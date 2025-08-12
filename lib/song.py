145ea8fdb73070bd199a40ecb1d029d98b5c6f7c
Rubil <mogererubil@gmail.com>
2025-08-12 15:17:50 +0300

    new commit

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,26 @@
+# song.py
+
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Update song count
+        Song.count += 1
+        
+        # Update genre tracking
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+        
+        # Update artist tracking
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
